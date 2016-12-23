eb4924921e60c17f1ed617ae573122d1ea78a4cd
vbulnes <victor.bulnes@gmail.com>
2016-12-23 14:06:08 +0100

    adding STATIC_ROOT to settings

--- SmartParking/settings.py
+++ SmartParking/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = '/static'
+STATIC_ROOT = 'home/smparking/smparking/static'
 
 STATIC_URL = '/static/'
 

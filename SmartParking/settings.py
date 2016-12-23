fee43e5225f971a7ec97b382195f64c8e2dc8f67
vbulnes <victor.bulnes@gmail.com>
2016-12-23 13:21:56 +0100

    adding pythonanywhere user name to the database name

--- SmartParking/settings.py
+++ SmartParking/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smartparking',
+        'NAME': 'smparking$smartparking',
         'USER': 'smparking',
         'PASSWORD': 'smp312SQL',
         'HOST': 'smparking.mysql.pythonanywhere-services.com',

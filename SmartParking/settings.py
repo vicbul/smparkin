580ec1326a226002ab9cf5bc0aa18cbfdfdf35f7
vbulnes <victor.bulnes@gmail.com>
2016-12-26 14:50:20 +0100

    commit before checkout on master

--- SmartParking/settings.py
+++ SmartParking/settings.py
@@ -81,32 +81,30 @@ WSGI_APPLICATION = 'SmartParking.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 # MySQL on PythonAnyWhere
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'smparking$smartparking',
+#         'USER': 'smparking',
+#         'PASSWORD': 'smp312SQL',
+#         'HOST': 'smparking.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#     }
+# }
+
+# MySQL on localhost
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smparking$smartparking',
-        'USER': 'smparking',
-        'PASSWORD': 'smp312SQL',
-        'HOST': 'smparking.mysql.pythonanywhere-services.com',
+        'NAME': 'smartparking',
+        'USER': 'spadmin',
+        'PASSWORD': 'spadmin',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
-
-# MySQL on localhost
-# DATABASES = {
-#     'default': {
-#         #'ENGINE': 'django.db.backends.sqlite3',
-#         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'smartparking',
-#         'USER': 'spadmin',
-#         'PASSWORD': 'spadmin',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
-# }
 
 
 # Password validation

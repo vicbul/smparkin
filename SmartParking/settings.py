01e776f30981e7e7e5d41b4cc0ad5b4f5718ce1b
vbulnes <victor.bulnes@gmail.com>
2017-01-18 12:41:52 +0100

    removing local connection from settings

--- SmartParking/settings.py
+++ SmartParking/settings.py
@@ -82,36 +82,36 @@ WSGI_APPLICATION = 'SmartParking.wsgi.application'
 
 # Check whether MySQL production server is reachable. Otherwise connect to localhost.
 data_connect = socket.socket()
-try:
-    host = 'smparking.mysql.pythonanywhere-services.com'
-    port = '3306'
-    data_connect.connect((host,int(port)))
-    # MySQL on PythonAnyWhere
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'smparking$smartparking',
-            'USER': 'smparking',
-            'PASSWORD': 'smp312SQL',
-            'HOST': host,
-            'PORT': port,
-        }
+# try:
+host = 'smparking.mysql.pythonanywhere-services.com'
+port = '3306'
+data_connect.connect((host,int(port)))
+# MySQL on PythonAnyWhere
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smparking$smartparking',
+        'USER': 'smparking',
+        'PASSWORD': 'smp312SQL',
+        'HOST': host,
+        'PORT': port,
     }
-except:
-    print 'Could not reach smparking MySQL server. Connecting to localhost...'
-    # MySQL on localhost
-    DATABASES = {
-        'default': {
-            #'ENGINE': 'django.db.backends.sqlite3',
-            #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'smartparking',
-            'USER': 'spadmin',
-            'PASSWORD': 'spadmin',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-        }
-    }
+}
+# except:
+#     print 'Could not reach smparking MySQL server. Connecting to localhost...'
+#     # MySQL on localhost
+#     DATABASES = {
+#         'default': {
+#             #'ENGINE': 'django.db.backends.sqlite3',
+#             #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'smartparking',
+#             'USER': 'spadmin',
+#             'PASSWORD': 'spadmin',
+#             'HOST': '127.0.0.1',
+#             'PORT': '3306',
+#         }
+#     }
 
 
 # Password validation
